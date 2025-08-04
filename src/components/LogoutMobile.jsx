8fd00d903d9e47d9609352aa0d1b2258c3ea4603
Chinmay Mehrotra <mehrotrachinmay6@gmail.com>
2025-08-04 20:59:41 +0530

    logout error fixed

--- src/components/LogoutMobile.jsx
+++ src/components/LogoutMobile.jsx
@@ -2,10 +2,11 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 
 const LogoutMobile = () => {
-  const { loginWithRedirect } = useAuth0();
+  const { logout } = useAuth0();
   return (
     <>
-    <button onClick={() => loginWithRedirect()} className="md:hidden text-sm text-black hover:cursor-pointer no-underline shadow-xl p-1.5 rounded font-semibold">
+    <button 
+    onClick={() => logout({ returnTo: window.location.origin })} className="md:hidden text-sm text-black hover:cursor-pointer no-underline shadow-xl p-1.5 rounded font-semibold">
       Log Out
     </button>
     </>
